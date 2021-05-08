239fb17fe495ce1f29c67b61c11d73d1abb12f67
Yasin Aydin <yasin@yasinaydin.net>
2021-05-08 23:10:08 +0300

    Add commander and basic opts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('hello world')
+import getOptions from "./lib/options";
+
+const options = getOptions();
+
+if (Object.keys(options).length === 0) {
+  console.log("Interactive mode is not developed yet.")
+  console.log("Please run the application with '-h' or '--help' parameter to see its usage.\n")
+  process.exit(0)
+}
