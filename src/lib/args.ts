2b937d0365b95d719e5fc81846f45b831f833ef5
Yasin Aydin <yasin@yasinaydin.net>
2021-05-15 01:25:14 +0300

    args: skip arg parsing on test

--- src/lib/args.ts
+++ src/lib/args.ts
@@ -2,14 +2,16 @@ import { Command } from 'commander'
 
 const program = new Command()
 
-program
-  .option('-r, --read_only', 'read only mode (does not modify logs)')
-  .option('-d, --debug', 'output extra debugging')
-  .option('-f, --file <logfile>', 'log file path')
-  .option('-t, --type <logtypetype>', 'log type (-T to list all types)')
-  // .option('-T, --list_types', 'list log types') // not implemented
+if (process.env.NODE_ENV != "test") {
+  program
+    .option('-r, --read_only', 'read only mode (does not modify logs)')
+    .option('-d, --debug', 'output extra debugging')
+    .option('-f, --file <logfile>', 'log file path')
+    .option('-t, --type <logtypetype>', 'log type (-T to list all types)')
+    // .option('-T, --list_types', 'list log types') // not implemented
 
-program.parse(process.argv)
+  program.parse(process.argv)
+}
 
 export const appArguments = program.opts()
 
