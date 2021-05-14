5cf0f9f8a05b3d474111c3261bf330247edd0ab6
Yasin Aydin <yasin@yasinaydin.net>
2021-05-14 19:20:57 +0300

    test: cleanup driver test

--- src/drivers/logagent.test.ts
+++ src/drivers/logagent.test.ts
@@ -1,20 +1,12 @@
-const Logagent = require('@sematext/logagent');
-
 // For more tests see:
 //  https://github.com/sematext/logagent-js/blob/master/test/test.js
 
-test('adds 1 + 2 to equal 3', done => {
+import { parseLine } from "./logagent";
+
+test('checks if driver works', async () => {
   const nginxline = '1.1.1.13 - - [04/May/2021:09:15:28 +0300] "GET /login HTTP/1.1" 200 8851 "https://test.local" Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.0; Trident/3.1)"';
 
-  const logAgent = new Logagent();
+  const response: any = await parseLine(nginxline, "nginx");
 
-  logAgent.parseLine(
-    nginxline,
-    'nginx',
-    (error, response) => {
-      // console.log({response})
-      expect(response.logSource).toBe('nginx');
-      done()
-    }
-  );
+  expect(response.logSource).toBe('nginx');
 });
