1bfa4c4f77c40818efe1407fda32a998c1397f3b
Yasin Aydin <yasin@yasinaydin.net>
2021-05-14 19:18:42 +0300

    lib: Put one space before errors

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -11,8 +11,9 @@ export function logAppend(logLevel: LogLevel, message: string) {
 export function log(logLevel: LogLevel, message: string, appendable: boolean = false) {
   if (logLevel == "debug" && !appArguments.debug) return;
 
-  const output = `[${logLevel}] ${message}`
-  
+  let output = `[${logLevel}] ${message}`
+  if (logLevel == "error") output = "\n" + output;
+
   if (appendable) process.stdout.write(output)
   else console[logLevel](output)
 }
